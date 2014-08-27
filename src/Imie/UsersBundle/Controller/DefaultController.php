2454d2e9b4fa1ed000b5a65ccc9eb9fbe8706327
Kévin Le Diouron <kaylediouron@gmail.com>
2014-08-27 11:37:10 +0200

    Ajout du bundle "UsersBundle"

---
+++ src/Imie/UsersBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Imie\UsersBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('UsersBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
